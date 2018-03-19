aed67c7cbd431ad5f83c32f98fce159099524eb5
kristenlau <kristenlau@gijs.dhcp.wustl.edu>
2018-03-19 15:53:24 -0500

    this doesn't work psa

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,43 @@
 package studio8;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 // TODO: Develop an algorithm to count steps in accelerometer data
 //    Major steeps:
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+public class CountSteps {
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		String file = "data/7Steps.csv";
+		File data = new File(file); 
+		Scanner scan = new Scanner(data);
+		
+		while(scan.hasNextLine()) {
+			String s = scan.nextLine();
+			String[] a = s.split(",");
+			System.out.println(a[2]);
+		}
+		//scan.useDelimiter(",");
+		/*for(int i = 0; scan.hasNext(); i ++) {
+			String s = scan.next();
+			if(i%3 ==0) {
+				System.out.println(s);
+			}
+		}	*/
+//		while(scan.hasNext()) {
+//		}
+		scan.close();
+	}
+	
+	public int Count() {
+		
+		
+		return 1;
+	}
+	
+	
+}
